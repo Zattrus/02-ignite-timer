6f7c311e52442e7011ffee7c56c38bfe00d8edb8
Gabriel Oliveira <oliveiragabriel1712@gmail.com>
2023-01-31 09:08:33 -0300

    Construindo rotas na aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-// import { useState } from "react";
 import { ThemeProvider } from "styled-components";
-
+import { BrowserRouter } from "react-router-dom";
 import { defaultTheme } from "./styles/theme/default";
 import { GlobalStyle } from "./styles/global";
+import { Router } from "./Router";
 
 export function App() {
-    // const [count, setCount] = useState(0);
-
     return (
         <ThemeProvider theme={defaultTheme}>
-            <h1>Hello World</h1>
-
+            <BrowserRouter>
+                <Router />
+            </BrowserRouter>
             <GlobalStyle />
         </ThemeProvider>
     );
