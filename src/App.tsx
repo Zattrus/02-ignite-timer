404016be04328c58cffcea02ffc3638540ef2301
Gabriel Oliveira <oliveiragabriel1712@gmail.com>
2023-01-30 22:19:32 -0300

    Estrutura da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import { useState } from 'react'
+// import { useState } from "react";
+import { ThemeProvider } from "styled-components";
+
+import { defaultTheme } from "./styles/theme/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
-    const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0);
 
     return (
-        <h1>Hello World</h1>
-    )
+        <ThemeProvider theme={defaultTheme}>
+            <h1>Hello World</h1>
+
+            <GlobalStyle />
+        </ThemeProvider>
+    );
 }
