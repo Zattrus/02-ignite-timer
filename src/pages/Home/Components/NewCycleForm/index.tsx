0ffd32df6e477383e3913a2424704f2993b27138
Gabriel Oliveira <oliveiragabriel1712@gmail.com>
2023-02-08 10:48:11 -0300

    Finalizando contexto react

--- src/pages/Home/Components/NewCycleForm/index.tsx
+++ src/pages/Home/Components/NewCycleForm/index.tsx
@@ -1,26 +1,11 @@
 import { FormContainer, MinutesAmountInput, TaskInput } from "./styles";
-import { zodResolver } from "@hookform/resolvers/zod";
-import * as zod from "zod";
-import { useForm } from "react-hook-form";
-
-const newCycleFormValidationSchema = zod.object({
-    task: zod.string().min(1, "Informe a tarefa"),
-    minutesAmount: zod
-        .number()
-        .min(5, "O ciclo precisa ser de no mínimo 5 minutos.")
-        .max(60, "O ciclo precisa ser de no máximo 60 minutos."),
-});
-
-type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
+import { useContext } from "react";
+import { useFormContext } from "react-hook-form";
+import { CycleContext } from "../../../../contexts/CyclesContext";
 
 export function NewCycleForm() {
-    const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
-        resolver: zodResolver(newCycleFormValidationSchema),
-        defaultValues: {
-            task: "",
-            minutesAmount: 0,
-        },
-    });
+    const { activeCycle } = useContext(CycleContext);
+    const { register } = useFormContext();
     return (
         <FormContainer>
             <label htmlFor="task">Vou trabalhar em</label>
